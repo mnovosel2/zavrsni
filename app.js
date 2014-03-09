154990e3c054b83b095de7b03de14c1e5d91a72d
Mario Novoselec <m.novoselec1@gmail.com>
2014-03-09 16:53:26 +0100

    Configured MongoDB database

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 
-/**
- * Module dependencies.
- */
+var express = require('express'),
+ 	routes = require('./routes'),
+ 	http = require('http'),
+ 	path = require('path'),
+ 	mongoose=require('mongoose'),
+	app = express();
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
+//Database setup
+var db=mongoose.connect('mongodb://localhost/zavrsniDB');
+var documentSchema=new mongoose.Schema({
+		title:String,
+		content:String,
+		tags:String,
+		dateCreated:{
+				type: Date,
+				default: Date.now
+		}
+});
+var documents=mongoose.model('documents',documentSchema);
+app.use(function(req,res,next){
+	req.documents=documents;
+	next();
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('less-middleware')(path.join(__dirname, '/public'),{compress:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.get('/', routes.index);
-app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
